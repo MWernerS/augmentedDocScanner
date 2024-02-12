d04ccf30ef63ef00163397a25ef50d9b1e162d4d
Blake Eastman <beastman@fastmail.com>
2024-02-12 11:20:44 -0500

    Feat: completed qr code detection and decoding
    This commit finishes the qr detection and decoding requirement of issue
    11.
    
    Addresses #11

--- ADS/controller/controller.js
+++ ADS/controller/controller.js
@@ -1,3 +1,15 @@
 console.log("controller loaded");
 
+function detectAndAlertCode(img)
+{
+    let printer = function (s) {console.log(s)};
+    qrcode.callback=qrCodeFound;
+    qrcode.decode(img);
+}
 
+function qrCodeFound(contents)
+{
+  console.log(contents);
+  let detective = new Detector(qrcode.grayScaleToBitmap(qrcode.grayscale()))
+  console.log(detective.detect());
+}
