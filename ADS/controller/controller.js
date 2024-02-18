953dfaf2a1b16b35c2331f99ba930384b6bec42c
Blake Eastman <beastman@fastmail.com>
2024-02-18 15:18:34 -0500

    QR detection unit tests
    editted controller to output run status
    editted qrtest java to run test
    editted view to output status for tests
    needs work interfacing test with qr detection
    
    addresses #11

--- ADS/controller/controller.js
+++ ADS/controller/controller.js
@@ -1,15 +1,17 @@
 console.log("controller loaded");
 
-function detectAndAlertCode(img)
-{
-    let printer = function (s) {console.log(s)};
-    qrcode.callback=qrCodeFound;
-    qrcode.decode(img);
-}
 
 function qrCodeFound(contents)
 {
   console.log(contents);
   let detective = new Detector(qrcode.grayScaleToBitmap(qrcode.grayscale()))
   console.log(detective.detect());
+}
+
+function detectAndAlertCode(img)
+{
+  console.log("controller operating")
+  let printer = function (s) {console.log(s)};
+  qrcode.callback=qrCodeFound;
+  qrcode.decode(img);
 }
