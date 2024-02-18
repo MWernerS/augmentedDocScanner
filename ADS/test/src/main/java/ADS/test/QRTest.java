f128b29f5b1d4853fcfec8c0bffbb97839a3aa69
Blake Eastman <beastman@fastmail.com>
2024-02-18 15:18:34 -0500

    Feat: QR detection unit tests
    editted controller to output run status
    editted qrtest java to run test
    editted view to output status for tests
    needs work interfacing test with qr detection
    
    addresses #11

--- ADS/test/src/main/java/ADS/test/QRTest.java
+++ ADS/test/src/main/java/ADS/test/QRTest.java
@@ -1,14 +1,36 @@
 package ADS.test;
 
 import com.microsoft.playwright.*;
+import java.nio.file.Paths;
+import java.lang.Thread;
 
 public class QRTest {
     public static void main(String[] args) {
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.firefox().launch();
             Page page = browser.newPage();
-            page.navigate("http://playwright.dev");
-            System.out.println(page.title());
+
+            page.onConsoleMessage(msg -> {
+                System.out.println("Console output: "+msg.text());
+            });
+
+            page.navigate("file:///home/blake/Documents/School/Senior%202/COSC%20481W/augmentedDocScanner/ADS/view/home.html");
+
+           
+            // Select one file
+            System.out.println("Uploading file...");
+            page.getByTestId("fileupload").setInputFiles(Paths.get("/home/blake/Documents/School/Senior 2/COSC 481W/augmentedDocScanner/ADS/test/qr1.png"));
+            System.out.println("Uploaded file");
+            //expectConsoleMessage( equalTo("https://emich.edu/") );
+
+            try {
+                Thread.sleep(3000);
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+
         }
     }
 }
