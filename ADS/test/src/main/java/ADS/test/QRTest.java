03f9fd04fbfc8457c5543c97539c32e146d22839
Blake Eastman <beastman@fastmail.com>
2024-02-18 20:25:18 -0500

    Finished writing the unit test
    Changed particular homepage elements to contain test ids
    Added element to homepage to display qr coordinates (will be removed in
    future)
    Added test qr1
    Added test.bash for easy test execution in unix
    QRTest.java fully functional
    Added maven target to gitignore
    
    Addresses #11

--- ADS/test/src/main/java/ADS/test/QRTest.java
+++ ADS/test/src/main/java/ADS/test/QRTest.java
@@ -3,34 +3,21 @@ package ADS.test;
 import com.microsoft.playwright.*;
 import java.nio.file.Paths;
 import java.lang.Thread;
+import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
 
 public class QRTest {
+
+    private static final boolean headless = true;
+
     public static void main(String[] args) {
         System.out.println("Working Directory = " + System.getProperty("user.dir"));
         try (Playwright playwright = Playwright.create()) {
-            Browser browser = playwright.firefox().launch();
+            Browser browser = playwright.firefox().launch(new BrowserType.LaunchOptions().setHeadless(headless));
             Page page = browser.newPage();
 
-            page.onConsoleMessage(msg -> {
-                System.out.println("Console output: "+msg.text());
-            });
-
             page.navigate("file:///home/blake/Documents/School/Senior%202/COSC%20481W/augmentedDocScanner/ADS/view/home.html");
-
-           
-            // Select one file
-            System.out.println("Uploading file...");
             page.getByTestId("fileupload").setInputFiles(Paths.get("/home/blake/Documents/School/Senior 2/COSC 481W/augmentedDocScanner/ADS/test/qr1.png"));
-            System.out.println("Uploaded file");
-            //expectConsoleMessage( equalTo("https://emich.edu/") );
-
-            try {
-                Thread.sleep(3000);
-            } catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-
+            assertThat(page.getByTestId("qrresult")).hasText("QR point coordinates: (94, 206), (94, 134), (166, 134)");
         }
     }
 }
