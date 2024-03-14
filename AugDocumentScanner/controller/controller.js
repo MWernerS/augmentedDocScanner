813891c0e9f5a5206ef67cf48df9821f75675f02
bsayre2 <145402642+bsayre2@users.noreply.github.com>
2024-03-14 15:14:44 -0400

    Feature: got measurements of QR code in pixels
    This commit adds to the crop function and gets the data for the measurements of the QR code encoded in the webpage. It uses this to get the QR code's measurements in pixels.
    
    *Added tokenizer to get encoded data
    *Added the algorithm to get the QR code measurements in pixels
    
    addresses #12

--- AugDocumentScanner/controller/controller.js
+++ AugDocumentScanner/controller/controller.js
@@ -32,6 +32,7 @@ function qrCoordsToPage(qr)
   else
   {
     let points = qr.points;
+    console.log(qr.bits);
     qrresultElement.innerHTML = `QR point coordinates: (${points[0].x}, ${points[0].y}), (${points[1].x}, ${points[1].y}), (${points[2].x}, ${points[2].y})`
   }
 }
@@ -43,8 +44,56 @@ function generatePdfEvent(images)
 
 function crop(images)
 {
+  let remaining = [images.length];
+
+  function cropCallback(encoded)
+  {
+    const params = encoded.split('?')[1].split('&');
+    let tokenParams = tokenize(params, "=");
+    if(tokenParams["w"] == undefined){
+      tokenParams["w"] = 8.5;
+    }
+    if(tokenParams["l"] == undefined){
+      tokenParams["l"] = 11;
+    }
+    if(tokenParams["qw"] == undefined){
+      tokenParams["qw"] = 1;
+    }
+    if(tokenParams["qx"] == undefined){
+      tokenParams["qx"] = 7;
+    }
+    if(tokenParams["qy"] == undefined){
+      tokenParams["qy"] = .5;
+    }
+
+    let detective = new Detector(qrcode.grayScaleToBitmap(qrcode.grayscale()));
+    let qrWidthInBits = detective.bits.Height+4;
+    let qrPoint0, qrPoint1;
+    let qrWidthInPixels = ((qrPoint1 - qrPoint0)/(qrWidthInBits-10)*(qrWidthInBits));
+    let pixelsPerInch = qrWidthInPixels/tokenParams["qw"];
+    let inchesPerPixel = tokenParams["qw"]/qrWidthInPixels;
+    
+  }
+
+  qrcode.callback=cropCallback;
+  for(let i=0; i<images.length;i++){
+
+  }
 
 }
+
+function tokenize(l, separator){
+
+  let output= [];
+  for (let i=0; i<l.length;i++){
+
+    let t = l[i].split(separator)
+    output[l[0]] = l[1];
+  }
+
+  return output;
+}
+
 
 function generatePDF(images)
 {
