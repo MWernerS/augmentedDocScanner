d9beb926faa4dbd5ab4c9ab1c1e25cde032a6cd8
Gerglefinn <gerglefinn@gmail.com>
2024-04-22 11:03:40 -0400

    fixed and tested generate pdf for pop up blockers

--- AugDocumentScanner/controller/controller.js
+++ AugDocumentScanner/controller/controller.js
@@ -362,13 +362,7 @@ async function generatePDF(images, pageSize) {
     })
   }
 
-  //previewPDF(pdfDoc);
-  const pdfBytes = await pdfDoc.save()
-  //pdfBytes is placed into a blob which can create a URL to be opened into a new tab
-  let blb = new Blob([pdfBytes], {type: 'application/pdf'});
-  let link = window.URL.createObjectURL(blb);
-  window.open(link);
-  return pdfDoc;
+  previewPDF(pdfDoc);
 }
 
 let pngImageBytes = "";
@@ -435,13 +429,13 @@ async function spawnQR(pageWidth, pageHeight, qrX, qrY, qrS) {
   });
 }
 
-/*async function previewPDF(pdfDoc) {
+async function previewPDF(pdfDoc) {
   const pdfBytes = await pdfDoc.save()
   //pdfBytes is placed into a blob which can create a URL to be opened into a new tab
   let blb = new Blob([pdfBytes], {type: 'application/pdf'});
   let link = window.URL.createObjectURL(blb);
   window.open(link);
-}*/
+}
 
 async function downloadPDF(pdfDoc, pdfName) {
   //PDFDocument to bytes (a Uint8Array)
